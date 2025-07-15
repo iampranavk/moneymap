d3f75514894bf7b20666d4c6eda7331407a41628
Pranav K <pranavk853@gmail.com>
2025-07-15 11:41:58 +0530

    app logo updated

--- lib/ui/splash_screen.dart
+++ lib/ui/splash_screen.dart
@@ -13,7 +13,7 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
-    Future.delayed(const Duration(seconds: 2), () {
+    Future.delayed(const Duration(seconds: 1), () {
       if (mounted) {
         Navigator.pushReplacement(
           context,
